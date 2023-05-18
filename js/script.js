d1c84098dcb1852147e40f38084237711795cf71
Felipe Oliveira <fbmithrandir@outlook.com>
2023-05-17 23:01:41 -0300

    perguntas frequentes e parametros url

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar links do menu
 const links = document.querySelectorAll(".header-menu a")
 
 function ativarLink (link) {
@@ -10,3 +11,44 @@ function ativarLink (link) {
 }
 
 links.forEach(ativarLink)
+
+//Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro)
+  if(elemento){
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto)
+
+//Perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button")
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls')
+  const resposta = document.getElementById(controls)
+
+  resposta.classList.toggle("ativa")
+
+  const ativa = resposta.classList.contains('ativa')
+  if (ativa){
+    pergunta.setAttribute('aria-expanded',true)
+  }
+  else {
+    pergunta.setAttribute('aria-expanded',false)
+  }
+
+}
+
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener('click',ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas)
+
+console.log(perguntas)
